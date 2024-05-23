7390ecf1694814cc4c5fdf56e203a0658ef9ee9f
BOAScripts <bottel.alexis@hotmail.com>
2024-05-23 23:54:27 +0200

    .ps1 updated

--- Set-DummyAD.ps1
+++ Set-DummyAD.ps1
@@ -1,47 +1,67 @@
-# Check local files presence
+# [1] Check local files presence
 
 $modelPath = ".\model.json"
 $usersCSVPath = ".\1000names.csv"
 
-if (!Test-Path $modelPath){
+if (!(Test-Path $modelPath)){
     Write-Host "$modelPath not present!" -ForegroundColor Red
     Exit
 }
-if (!Test-Path $usersCSVPath){
+else {
+    # IMPORT JSON in context
+}
+if (!(Test-Path $usersCSVPath)){
     Write-Host "$usersCSVPath not present!" -ForegroundColor Red
     Exit
 }
+else {
+    # IMPORT CSV in context
+}
 
-# Check ADDS role
+# [2] Check/Install ADDS role
 if ((Get-WindowsFeature AD-Domain-Services).Installed){
     Write-Host "[OK] ADDS already installed" -ForegroundColor Green
-    ## Get domain.tld + domain DN
+    # Get domain.tld + domain DN
+    try { 
+        $domain = (Get-ADDomain).Forest
+        $domainDN = (Get-ADRootDSE).rootDomainNamingContext
+    }
+    catch{
+        write-host $_ -ForegroundColor Red
+        write-host "ADDS installed but no domain detected - is the server in a 'WAITING TO PROMOTE' state ?"
+    }
 }
 else {
     Write-Host "[?] ADDS not installed" -ForegroundColor Yellow
-    $rep = Read-Host " [?] Do you want to install Active Directory Domain and Services ?"
+    $rep = Read-Host " [?] Do you want to install Active Directory Domain and Services ? (y/n)"
     if ($rep -like 'y*'){
+        # !!!!! GET PSW FROM MODEL !!!!!
         $DSRMpsw = ConvertTo-SecureString "Test1234=" -AsPlainText -Force
-        $domain = Read-Host "Enter domain name (domain.tld)"
-        Write-Host "[+] Installing ADDS role" -ForegroundColor Yellow
+        $domain = Read-Host "Please enter domain name (domain.tld)"
+        Write-Host "[+] Trying to install ADDS role" -ForegroundColor Yellow
         try{
             Install-WindowsFeature AD-Domain-Services -IncludeAllSubFeature -IncludeManagementTools
         }
-        catch{write-host $_}
-        Write-Host "[+] Promote server as Domain Controller"
+        catch{write-host $_ -ForegroundColor Red}
+        Write-Host "[+] Trying to promote server as Domain Controller" -ForegroundColor Yellow 
         try{
             Install-ADDSForest -DomainName $domain -InstallDns -SafeModeAdministratorPassword $DSRMpsw        
         }
-        catch{write-host $_}
+        catch{write-host $_ -ForegroundColor Red}
     }
-    else {exit}
+    else {
+        Write-Host "[?] You don't want to install ADDS - Exiting" -ForegroundColor Yellow
+        Exit
+    }
 }
 
+# [3] ZHU-LI, do the thing
 
-### Install-WindowsFeature AD-Domain-Services -IncludeAllSubFeature -IncludeManagementTools
-### Install-ADDSForest -DomainName $domain -InstallDns -SafeModeAdministratorPassword $DSRM
+# [3.1] Get Depts
 
-# 2. import files present ?
-## ERROR + STOP if not
+# [3.1.1] Set SharedFolder % Depts
 
-# 3. 
+# [3.1.2] Set DLGS groups
+
+# [3.1.3] Set GGS groups + membership of correspoding DLGS
+
