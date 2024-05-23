02715d3cebe5cec4e319efcd59d1552d64f5bfc1
BOAScripts <bottel.alexis@hotmail.com>
2024-05-23 23:20:30 +0200

    .ps1 updated

--- Set-DummyAD.ps1
+++ Set-DummyAD.ps1
@@ -1,22 +1,43 @@
-# 1. DETECT ADDS / install ADDS
+# Check local files presence
 
-if (Get-WindowsFeature AD-Domain-Services).Installed){
+$modelPath = ".\model.json"
+$usersCSVPath = ".\1000names.csv"
+
+if (!Test-Path $modelPath){
+    Write-Host "$modelPath not present!" -ForegroundColor Red
+    Exit
+}
+if (!Test-Path $usersCSVPath){
+    Write-Host "$usersCSVPath not present!" -ForegroundColor Red
+    Exit
+}
+
+# Check ADDS role
+if ((Get-WindowsFeature AD-Domain-Services).Installed){
     Write-Host "[OK] ADDS already installed" -ForegroundColor Green
+    ## Get domain.tld + domain DN
 }
 else {
     Write-Host "[?] ADDS not installed" -ForegroundColor Yellow
     $rep = Read-Host " [?] Do you want to install Active Directory Domain and Services ?"
     if ($rep -like 'y*'){
-        $installADDS = $true
+        $DSRMpsw = ConvertTo-SecureString "Test1234=" -AsPlainText -Force
+        $domain = Read-Host "Enter domain name (domain.tld)"
+        Write-Host "[+] Installing ADDS role" -ForegroundColor Yellow
+        try{
+            Install-WindowsFeature AD-Domain-Services -IncludeAllSubFeature -IncludeManagementTools
+        }
+        catch{write-host $_}
+        Write-Host "[+] Promote server as Domain Controller"
+        try{
+            Install-ADDSForest -DomainName $domain -InstallDns -SafeModeAdministratorPassword $DSRMpsw        
+        }
+        catch{write-host $_}
     }
     else {exit}
 }
 
-## domain.tld
-## OR
-## install ADDS
-### Get DSRM psw
-### Get domain (dom.tld)
+
 ### Install-WindowsFeature AD-Domain-Services -IncludeAllSubFeature -IncludeManagementTools
 ### Install-ADDSForest -DomainName $domain -InstallDns -SafeModeAdministratorPassword $DSRM
 
