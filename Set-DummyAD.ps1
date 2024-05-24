e07643c43358e43ed353447755c810e5fcdcfec6
BOAScripts <bottel.alexis@hotmail.com>
2024-05-24 17:40:24 +0200

    ps1 updates: [1] [2] steps

--- Set-DummyAD.ps1
+++ Set-DummyAD.ps1
@@ -1,26 +1,22 @@
-# [1] Check local files presence
+# VARS definition
 
 $modelPath = ".\model.json"
 $usersCSVPath = ".\1000names.csv"
 
-if (!(Test-Path $modelPath)){
-    Write-Host "$modelPath not present!" -ForegroundColor Red
+
+# [1] Check local files presence and import in context
+if (!(Test-Path $modelPath) -or !(Test-Path $usersCSVPath)){
+    Write-Host "$modelPath or $usersCSVPath not present!" -ForegroundColor Red
     Exit
 }
 else {
-    # IMPORT JSON in context
-}
-if (!(Test-Path $usersCSVPath)){
-    Write-Host "$usersCSVPath not present!" -ForegroundColor Red
-    Exit
-}
-else {
-    # IMPORT CSV in context
+    $model = Get-Content $modelPath | ConvertFrom-Json
+    $CSVNames = [System.Collections.ArrayList](Get-Content $usersCSVPath | ConvertFrom-Csv -Delimiter ";")
 }
 
 # [2] Check/Install ADDS role
 if ((Get-WindowsFeature AD-Domain-Services).Installed){
-    Write-Host "[OK] ADDS already installed" -ForegroundColor Green
+    Write-Host "[✔] ADDS already installed" -ForegroundColor Green
     # Get domain.tld + domain DN
     try { 
         $domain = (Get-ADDomain).Forest
@@ -35,20 +31,18 @@ else {
     Write-Host "[?] ADDS not installed" -ForegroundColor Yellow
     $rep = Read-Host " [?] Do you want to install Active Directory Domain and Services ? (y/n)"
     if ($rep -like 'y*'){
-        # !!!!! GET PSW FROM MODEL !!!!!
-        $DSRMpsw = ConvertTo-SecureString "Test1234=" -AsPlainText -Force
-        # !!!!! GET PSW FROM MODEL !!!!!
+        $DSRMpsw = ConvertTo-SecureString $model.PSW -AsPlainText -Force
         $domain = Read-Host "Please enter domain name (domain.tld)"
-        Write-Host "[+] Trying to install ADDS role" -ForegroundColor Yellow
         try{
+            Write-Host "[+] Trying to install ADDS role" -ForegroundColor Yellow
             Install-WindowsFeature AD-Domain-Services -IncludeAllSubFeature -IncludeManagementTools
+            Write-Host "[+] Trying to promote server as Domain Controller" -ForegroundColor Yellow 
+            Install-ADDSForest -DomainName $domain -InstallDns -SafeModeAdministratorPassword $DSRMpsw
+            Write-Host "[!] You'll be disconnected to login again with the domain Administrator (same password as local Adminstrator) " -ForegroundColor Yellow
         }
         catch{write-host $_ -ForegroundColor Red}
-        Write-Host "[+] Trying to promote server as Domain Controller" -ForegroundColor Yellow 
-        try{
-            Install-ADDSForest -DomainName $domain -InstallDns -SafeModeAdministratorPassword $DSRMpsw        
-        }
-        catch{write-host $_ -ForegroundColor Red}
+        
+
     }
     else {
         Write-Host "[?] You don't want to install ADDS - Exiting" -ForegroundColor Yellow
@@ -56,15 +50,15 @@ else {
     }
 }
 
-# [3] ZHU-LI, do the thing
+# [3] ZHU-LI, do the thing (Populate AD)
 
 ## Create OUs
 
 ### Root OU
 
-### Custom OUs
+### Custom OUs in model.json
 
-## foreach Depts in model.json
+### foreach Depts in model.json -> create an OU in Users
 
 ### Set GGS groups
 
