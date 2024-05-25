60ff43483c3d411b0e8882a0d01cf3d564f14ff1
BOAScripts <bottel.alexis@hotmail.com>
2024-05-25 10:04:56 +0200

    share

--- Set-DummyAD.ps1
+++ Set-DummyAD.ps1
@@ -116,9 +116,9 @@ try {
             # Write-Host "New-Item -Name $($dept.Name) -ItemType Directory -Path $($model.RootSharePath) | Out-Null"
             # Write-Host "New-SmbShare -Name $($dept.value) -Path $DeptSharePath | Out-Null"
             New-Item -Name $dept.Name -ItemType Directory -Path $model.RootSharePath | Out-Null
-            Start-Sleep -Seconds 1
+            Start-Sleep -Milliseconds 500
             New-SmbShare -Name $dept.value -Path $DeptSharePath | Out-Null
-            Start-Sleep -Seconds 1
+            Start-Sleep -Milliseconds 500
             Write-Host "        [+] $($dept.Name) Share directory" -ForegroundColor Yellow
         }
         ### Set ACLs to SharedFolder: DLGS (RO & RW)
